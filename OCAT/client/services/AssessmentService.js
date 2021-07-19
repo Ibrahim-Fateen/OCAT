d6cbe4f3c8030ee4c8e9b86953dd281df8f4f1d9
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2021-07-19 16:55:58 -0400

    Task 1 step 5

--- OCAT/client/services/AssessmentService.js
+++ OCAT/client/services/AssessmentService.js
@@ -6,7 +6,20 @@ export class AssessmentService {
       // Choose the correct method, url, and data to send
       // in a request to the express OCAT/server/routes/Assessment/index.js
       // NOTE: the http.config file automatically adds /api to the front of your url
-      return Axios.METHOD(`/some-url`, { })
+
+      let score = assessment.previous.value();
+      score += assessment.catAlteractoins.value() + assessment.ownerAlteractons.value();
+      score += assessment.hisses.value() + assessment.dogs.value();
+
+      return Axios.METHOD(`/api/assessment/submit`, {
+        data: {
+          birth: assessment.birth.value(),
+          cat: assessment.cat.value(),
+          instrument: assessment.instrument.value(),
+          score,
+        },
+        method: `post`,
+      })
         .then(response => response.data);
     }
     catch (err) {
