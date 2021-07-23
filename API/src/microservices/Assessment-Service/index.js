672e7cab10536a4ecf4672c1ef58afd1f7b15db3
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2021-07-23 16:16:06 -0400

    Add data to database

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -3,6 +3,15 @@ const { Assessments } = require(`../Database`);
 exports.submit = async (assessment) => {
   // use the bookshelf model Assessments from API/src/microservices/Database to save
   // the assessment data in the PostgreSQL database
+  const { score } = assessment;
+  const risk_level = score;
+  const { cat } = assessment;
+  const { birth } = assessment;
+
+  Assessments.save({
+    score, risk_level, cat_name: cat, cat_date_of_birth: birth,
+    created_at: Date.now(),
+  });
 };
 
 exports.getList = () => {
