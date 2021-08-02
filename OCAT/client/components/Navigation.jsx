2848166f0f84bb9da79ce03051995bb92ab1ab10
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2021-08-02 09:31:22 -0400

    Update Navigation.jsx

--- OCAT/client/components/Navigation.jsx
+++ OCAT/client/components/Navigation.jsx
@@ -4,11 +4,10 @@ import { Nav, Navbar } from 'react-bootstrap';
 export const Navigation = () =>
   <header>
     <Navbar expand="md" bg="dark" variant="dark" fixed="top">
-      <Navbar.Brand href="/">OCAT</Navbar.Brand>
+      <Navbar.Brand href="/assessment/list">OCAT</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="/">Dashboard</Nav.Link>
+        <Nav.Link href="/assessment/list">Dashboard</Nav.Link>
         <Nav.Link href="/assessment/new">New Assessment</Nav.Link>
-        <Nav.Link href="/assessment/list">Assessments List</Nav.Link>
       </Nav>
     </Navbar>
   </header>;
